f55fc8e668ca4ea48482166bf709ae7901e62ace
webber2408 <webcrat.tech@gmail.com>
2017-03-26 15:53:21 +0530

    completed tasks

---
+++ task1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+def function1():
+    fp = open("data_set.txt", "r")
+    print(fp.name)
+    cortana = 0
+    siri = 0
+    # METHOD 1
+    buffer = fp.readlines();
+    for line in buffer:
+        if "cortana" in line:
+            cortana = cortana + 1
+        elif "siri" in line:
+            siri = siri + 1
+    print(cortana)
+    print(siri)
+
+    # METHOD 2
+    # buffer = fp.read();
+    # buffer = buffer.split("\n")
+    # print(buffer)
+    # for line in buffer:
+    #     if "cortana" in line:
+    #         cortana = cortana + 1
+    #     elif "siri" in line:
+    #         siri = siri + 1
+    # print(cortana)
+    # print(siri)
+
+
+    fp.close()
+
+function1()
---
+++ task2.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+def function1():
+    fp = open("data_set.txt", "r")
+    # print(fp.name)
+    cortana = []
+    siri = []
+    task = 0
+    taskListForCortana=[]
+    statementListForCortana=[]
+    statement = 0
+
+
+    buffer = fp.read();
+    buffer = buffer.split("\n")
+    # print(buffer)
+    for line in buffer:
+        if "cortana" in line:
+            cortana.append(line)
+        elif "siri" in line:
+            siri.append(line)
+    print(cortana)
+    print(siri)
+
+    print("\n")
+
+    for line in cortana:
+        if "what" in line or "how" in line or "when" in line or "why" in line:
+            # print(line)
+            taskListForCortana.append(line);
+            task=task+1
+        else:
+            statementListForCortana.append(line)
+            statement=statement+1
+
+    print("TASKS FOR CORTANA")
+    print(taskListForCortana)
+    print("TOTAL = ",task)
+    print("STATEMENTS FOR CORTANA")
+    print(statementListForCortana)
+    print("TOTAL = ", statement)
+    print("\n")
+
+    task=0
+    statement=0
+    taskListForSiri=[]
+    statementListForSiri=[]
+
+    for line in siri:
+        if "what" in line or "how" in line or "when" in line or "why" in line:
+            # print(line)
+            taskListForSiri.append(line)
+            task=task+1
+        else:
+            statementListForSiri.append(line)
+            statement=statement+1
+
+    print("TASKS FOR SIRI")
+    print(taskListForSiri)
+    print("TOTAL = ", task)
+    print("STATEMENTS FOR SIRI")
+    print(statementListForSiri)
+    print("TOTAL = ", statement)
+    fp.close()
+
+function1()
+
+
